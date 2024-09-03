cac54c78ae621be383e60b0b6607c7d19c95fad2
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-09-03 13:20:59 +0200

    Rework cpputils_global.h header.

--- include/cpputils/cpputils_global.h
+++ include/cpputils/cpputils_global.h
@@ -1,12 +1,19 @@
 #pragma once
 
+// clang-format off
 
 #if defined(_WIN32) || defined(_WIN64)
-    #ifdef CPPUTILS_LIBRARY
-        #define CPPUTILS_API __declspec(dllexport)
+    #ifdef CPPUTILS_SHARED_LIB
+        #ifdef CPPUTILS_BUILD_LIB
+            #define CPPUTILS_API __declspec(dllexport)
+        #else
+            #define CPPUTILS_API __declspec(dllimport)
+        #endif
     #else
-        #define CPPUTILS_API __declspec(dllimport)
+        #define CPPUTILS_API
     #endif
 #else
     #define CPPUTILS_API
 #endif
+
+// clang-format onS
